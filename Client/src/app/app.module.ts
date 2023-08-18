531b0166e755c7ad5ce520a8e93709527e4529e0
angular_node <creqod123@gmail.com>
2023-08-18 18:06:01 +0530

    install components

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './user/home/home.component';
 import { CartComponent } from './user/cart/cart.component';
+import { ExtraComponent } from './extra/extra.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CartComponent
+    CartComponent,
+    ExtraComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,

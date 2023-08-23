e7f5697a00737ccfeb447a0a2ac884fae3813562
angular_node <creqod123@gmail.com>
2023-08-23 18:20:03 +0530

    new component

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,20 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './user/home/home.component';
-import { CartComponent } from './user/cart/cart.component';
-import { ExtraComponent } from './extra/extra.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    CartComponent,
-    ExtraComponent,
-    LoginComponent,
-    RegisterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

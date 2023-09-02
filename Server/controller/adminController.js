19a743ce11406ebd1c3a95bd1e2c72102d35dbbb
angular_node <creqod123@gmail.com>
2023-09-02 09:50:41 +0530

    some update

--- Server/controller/adminController.js
+++ Server/controller/adminController.js
@@ -47,34 +47,6 @@ exports.update = (async (req, res, next) => {
     }
 });
 
-// ============================= Admin add product =========================== 
-
-exports.add = (async (req, res, next) => {
-
-    try {
-        const form = req.body;
-        await adminProduct.create({
-            image: 'dasdasdasdasdsd.png',
-            productName: form.productName,
-            price: form.price,
-            stock: form.stock,
-            adminId: req.user._id
-        })
-
-        res.status(200).json({
-            success: true,
-            message: "complete",
-            data: req.body,
-        })
-    }
-    catch (error) {
-        res.status(404).json({
-            success: false,
-            message: error.message,
-        })
-    }
-});
-
 // ============================= Admin remove product =========================== 
 
 exports.remove = (async (req, res, next) => {
@@ -101,6 +73,34 @@ exports.remove = (async (req, res, next) => {
         res.status(404).json({
             success: false,
             message: "fail",
+        })
+    }
+});
+
+// ============================= Admin add product =========================== 
+
+exports.add = (async (req, res, next) => {
+
+    try {
+        const form = req.body;
+        await adminProduct.create({
+            image: 'dasdasdasdasdsd.png',
+            productName: form.productName,
+            price: form.price,
+            stock: form.stock,
+            adminId: req.user._id
+        })
+
+        res.status(200).json({
+            success: true,
+            message: "complete",
+            data: req.body,
+        })
+    }
+    catch (error) {
+        res.status(404).json({
+            success: false,
+            message: error.message,
         })
     }
 });
@@ -143,96 +143,10 @@ exports.delete = (async (req, res, next) => {
     }
 });
 
-// ============================= Admin Order show =========================== 
-
-exports.order = (async (req, res, next) => {
-    try {
-        const data = await address.find({ _id: req.body.email })
-        res.status(200).json({
-            success: true,
-            message: "complete",
-            data: data,
-        })
-    }
-    catch (error) {
-        res.status(404).json({
-            success: false,
-            message: "fail",
-        })
-    }
-});
-
 // ============================= Admin status update show =========================== 
-
 
 exports.status = (async (req, res, next) => {
     try {
-        const id = req.body.id
-        const status = req.body.status
-
-        if (status === 'conform') {
-            await checkout.updateOne({ _id: id }, { status: 'Conform' })
-            socket.conformOrder('conformOrder');
-        }
-        else {
-
-            const a = await checkout.findOne({ _id: id }).populate('addressId')
-            await checkout.deleteOne({ _id: id })
-            const check = a.addressId._id
-            const b = await checkout.find({ addressId: check })
-
-            if (b.length === 0) {
-                await address.deleteOne({ _id: check })
-            }
-        }
-        res.status(200).json({
-            success: true,
-            message: "complete",
-        })
-    }
-    catch (error) {
-        res.status(404).json({
-            success: false,
-            message: "fail",
-        })
-    }
-});
-
-// ============================= search added product =========================== 
-
-exports.search = (async (req, res, next) => {
-    try {
-        const Data = {}
-        const pageNumber = req.body.paginat
-        const _id = req.body._id
-        const message = req.body.message
-        const totalPosts = await adminProduct.find({ productName: message, adminId: _id }).countDocuments().exec();
-        let startIndex = pageNumber * 9;
-        Data.totalPosts = totalPosts;
-        Data.data = await adminProduct.find({ productName: message, adminId: _id })
-            .sort("-_id")
-            .skip(startIndex)
-            .limit(9)
-            .exec();
-        res.status(200).json({
-            success: true,
-            message: "complete",
-            data: Data
-        })
-    }
-    catch (error) {
-        res.status(404).json({
-            success: false,
-            message: "fail",
-        })
-    }
-});
-
-// ============================= search added product =========================== 
-
-exports.stock = (async (req, res, next) => {
-    try {
-        const a = await adminProduct.updateOne({ _id: req.body._id }, { stock: req.body.stock })
         res.status(200).json({
             success: true,
             message: "complete",
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -30,10 +30,7 @@ router.post('/seller/add', verifyToken, adminController.add); //
 router.get('/seller/detail', verifyToken, adminController.detail); //
 router.post('/seller/delete', verifyToken, adminController.delete); //
 
-router.post('/seller/order', verifyToken, adminController.order);
 router.post('/seller/status', verifyToken, adminController.status);
-router.post('/seller/search', verifyToken, adminController.search);
-router.post('/seller/stock', verifyToken, adminController.stock);
 // router.post('/seller/add', verifyToken, upload.single('image'), adminController.add);
 
 //            ============ User ============== 
